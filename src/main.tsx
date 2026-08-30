361bd18ee77a9f3c6d7226da4aa50c725122c5fe
luisfebg <52917070+luisfebg@users.noreply.github.com>
2026-08-30 03:09:14 +0100

    Enable V6.2 interactive chat UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { installChatViewportFixes } from './lib/chatViewport';
+import { installInteractiveChat } from './lib/interactiveChat';
 import './index.css';
 import './mobile-fixes.css';
+import './chat-interactions.css';
 
 installChatViewportFixes();
+installInteractiveChat();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
